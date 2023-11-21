9ecd5c826a69eab31d140931119657a7aa75b5bb
stevD9 <steven.t.drea@gmail.com>
2023-11-21 19:43:41 +0200

    init

---
+++ src/main/java/me/stef/bluemooncity/BlueMoonCityApplication.java
@@ -0,0 +1,13 @@
+package me.stef.bluemooncity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlueMoonCityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlueMoonCityApplication.class, args);
+	}
+
+}
